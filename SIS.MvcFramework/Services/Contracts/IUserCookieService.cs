8da1349a7c09f2301def02d64e3ad85ccc0e1478
Kristian <kriskobota@abv.bg>
2019-05-15 11:45:33 +0300

    Removed last project

--- SIS.MvcFramework/Services/Contracts/IUserCookieService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SIS.MvcFramework.Services.Contracts
-{
-    public interface IUserCookieService
-    {
-        string GetUserCookie(string username);
-
-        string GetUserData(string cookieContent);
-    }
-}
